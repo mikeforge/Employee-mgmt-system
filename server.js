b47f1f0c80c4faa2fb791a90869aaa379a72b3b7
Mike Kelly <mike.forge@gmail.com>
2022-03-09 21:32:34 -0500

    looking at some different options for building schema/seeding

--- server.js
+++ server.js
@@ -1,34 +1,104 @@
 const mysql = require('mysql2');
-const dotenv = require('dotenv');
+require('dotenv').config();
 const consoleTable = require('console.table');
-const connection = require('mysql2/typings/mysql/lib/Connection');
+// const connection = require('mysql2/typings/mysql/lib/Connection');
 const fs = require('fs');
 
-const seedQuery = fs.readFileSync('./seeds.sql', {
-    encoding: 'utf-8',
-})
+// const seedQuery = fs.readFileSync('./db/seeds.sql', {
+//     encoding: 'utf-8',
+// })
 
-const db = mysql.createConnection(
+let connection;
+ async function connect () {
+     connection = mysql.createConnection(
     {
         host: process.env.host,
         user: process.env.user,
         password: process.env.password,
-        database: process.env.database
-    },
-    console.log(`Connected to the employee_db database`)
-)
+        database: process.env.database,
+        multipleStatements: 'true'
+    });
+    console.log(`Connected to the employee_db database`);
+    await connection.query(fs.readFileSync('./db/schema.sql'));
+    console.log(`schema defined`);
+    init();
+ };
 
 
-connection.connect()
-
-console.log(`running sql seed..`)
-
-connection.query(seedQuery, err => {
-    if (err) {
-        throw err
-    }
-
-    console.log(`SQL seed complete`)
-});
+// connection.query(fs.readFileSync('./db/schema.sql'));
+// connection.query(fs.readFileSync('./db/seeds.sql'));
 
 
+// connection.connect()
+
+// console.log(`running sql seed..`)
+
+// connection.query(seedQuery, err => {
+//     if (err) {
+//         throw err
+//     }
+
+//     console.log(`SQL seed complete`);
+//     init();
+// });
+
+
+function init() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "choices",
+                message: "What would you like to do? Choose the option from below",
+                choices: [
+                    "Add a new department",
+                    "Add a new role",
+                    "Add a new employee",
+                    "View all department",
+                    "View all role",
+                    "View all employee",
+                    "Update employee role",
+                    "Exit",
+                ],
+            },
+        ])
+
+        .then((answer) => {
+            console.log(`Would you like to: ${answer.choices}`);
+            switch (answer.choices) {
+                case "View all employees":
+                    viewEmployees();
+                    break;
+                case "View all roles":
+                    viewRoles();
+                    break;
+                case "View all departments":
+                    viewDepartments();
+                    break;
+                case "Add a new department":
+                    addDepartment();
+                    break;
+                case "Add a new role":
+                    addRole();
+                    break;
+                case "Add a new employee":
+                    addEmployee();
+                    break;
+                case "Update employee role":
+                    updateEmployeeRole();
+                    break;
+                default:
+                    exit();
+            }
+        });
+}
+
+function viewEmployees () {
+    const sqlQuery = "select * from employee";
+    connection.query(sqlQuery, (err, res) => {
+     if (err) throw err;   
+     console.log(`show employees`);
+     console.table(res);
+    });
+
+}
